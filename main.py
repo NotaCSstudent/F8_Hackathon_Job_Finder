b7796bbcc7a6d928cbd670a87d346c8770686f0b
dev-rb <43100342+dev-rb@users.noreply.github.com>
2021-05-07 23:20:51 -0400

    Webhook setup for facebook messenger. And audio conversion from mp4 to wav. Need to download file from request.json attachment link.

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, abort
+from flask_ngrok import run_with_ngrok
+from pydub import AudioSegment
+
+app = Flask(__name__)
+
+file = AudioSegment.from_file("./sound.mp4", format="mp4")
+output = file.export("./output.wav", format="wav")
+
+
+@app.route('/test')
+def webhook():
+    VERIFY_TOKEN = "test"
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+
+    if mode and token:
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            print("Worked")
+            return challenge
+
+    return 'success', 200
+
+
+@app.route('/test', methods=['POST'])
+def handleMessage():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
+
+
+if __name__ == "__main__":
+    app.run()
