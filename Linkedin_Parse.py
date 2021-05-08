90c52852a3d503839831774bff21631e5bbdf443
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-08 14:58:02 -0400

    Delete Linkedin_Parse.py

--- Linkedin_Parse.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import bs4 as bs
-
-
-Job_Finder = urllib.request.urlopen('https://www.indeed.com/jobs?q=software+engineer&l=New+York%2C+NY')
-## We are using Indeed cause screw LinkedIn, we will be scraping stuff from Linkedin
-Job_Soup = bs.BeautifulSoup(Job_Finder,'xml')
-
-#print(Job_Soup)
