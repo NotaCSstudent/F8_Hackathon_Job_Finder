eebac866c594c071ea6dfe4a10a7cd6c8227f374
Yash Mahtani <ymahtani5911@bths.edu>
2021-05-07 22:59:33 -0400

    Added mp4 to wav converter

--- f8h.py
+++ f8h.py
@@ -1,5 +1,7 @@
 import speech_recognition as sr
 import os
+import converter
+
 
 def wav2txt(audioFileName):
     reco = sr.Recognizer()
@@ -16,9 +18,19 @@ def wav2txt(audioFileName):
     txtFile.write(reco.recognize_wit(givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
     txtFile.close()
 
+def clip2wav(audioFileName):
+    c = Converter()
+    conv = c.convert(audioFileName, 'audio.mp3', {'format':'mp3','audio':{'codec': 'mp3','bitrate':'22050','channels':1}})
+    for timecode in conv:
+        pass    
+    os.system("mpg123 -w audio.wav audio.mp3")
+    wav2txt("audio.wav")
+
+
 def main():
     audFile = input("Enter audio file name: ")
-    wav2txt(audFile)
+    clip2wav(audFile)
+    
 
 if __name__ == "__main__":
     main()
