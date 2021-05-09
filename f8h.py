985def861e74c67d488f8103eb93244d2274e572
NotaCSstudent <mahmudbabu94@gmail.com>
2021-05-09 19:03:23 -0400

    Add files via upload
    audio recognition

--- f8h.py
+++ f8h.py
@@ -1,6 +1,83 @@
 import speech_recognition as sr
 import os
-import converter
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+## Begin Job Finder
+
+
+def Get_Url(Job :str):
+    Template = 'https://www.indeed.com/jobs?q={}&l=New+York%2C+NY'
+    
+    Url = Template.format(Job)
+    
+    return Url
+
+
+
+
+
+def Find_Job(Job : str,Number_Of_Jobs :int):
+    
+    Job_Url = Get_Url(Job) ##For the Helper Function Above
+    
+    Job_Response = requests.get(Job_Url) ## Getting a Response from the URL
+    
+    Job_Soup = BeautifulSoup(Job_Response.text,"html.parser") ##Parses into an HTML Tree Structure
+    
+    Name_Of_Role = Job_Soup.find_all('div','jobsearch-SerpJobCard')##Role name tag
+    
+    Name_Finder = Name_Of_Role[Number_Of_Jobs]
+    
+    Name = Name_Finder.h2.a
+    
+    Company_Name = Job_Soup.find_all('span','company') ##Finds company html tag
+    
+    Company = Name_Finder.find('span','company').text.strip() ## Gets the company name and strips out anything thats not needed
+    
+    Company_Location = Name_Of_Role[Number_Of_Jobs] ##Starting at index 
+
+    Location = Company_Location.find('div','recJobLoc').get('data-rc-loc')##gets the role
+
+    Details = Name['title'] + "\n" + Company + "\n" + Location
+
+    return (Details)
+
+
+#print(len(Find_Job("Software Engineer")))
+##First_Job = Find_Job("Software Engineer",1) 
+#print(len(First_Job))
+##print(First_Job[0])
+#print(First_Job[0].h2)
+#print(First_Job[0].h2.a)
+#Tag = First_Job[0].h2.a
+#print(Tag['title'])
+
+
+
+
+def Find_My_Job(Insert_Job : str):
+    for i in range(1,7):
+        try:
+            print(Find_Job(Insert_Job, i));
+        except(IndexError):
+            pass
+        ##Use this to skip any Index Errors
+##Find_My_Job("Software Engineer")
+##Find_My_Job("Data Analyst")
+##Find_My_Job("Janitor")
+Find_My_Job("Tutor")
+
+
+## End Job Finder
+
+
+
+
+
+## Begin wav to text
 
 
 def wav2txt(audioFileName):
@@ -12,25 +89,25 @@ def wav2txt(audioFileName):
     with givenAudioFile as srcFile:
         givenAudioFile = reco.record(srcFile)
 
-    print(reco.recognize_wit(givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
-
+    userCommand = (reco.recognize_wit(givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
+    if userCommand.find(' in') > -1:
+        userCommand = userCommand[ (userCommand.find('looking for ')+ 12) : (userCommand.find(' in'))]
+    else:
+        userCommand = userCommand[ (userCommand.find('looking for ')+ 12) :]
+    if userCommand.find('recent ') > -1:
+        userCommand = userCommand[ (userCommand.find('recent ')+ 7) :]
+    
+    
     txtFile = open("audioAsText.txt", "w+")
-    txtFile.write(reco.recognize_wit(givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
+    txtFile.write(userCommand)
     txtFile.close()
 
-def clip2wav(audioFileName):
-    c = Converter()
-    conv = c.convert(audioFileName, 'audio.mp3', {'format':'mp3','audio':{'codec': 'mp3','bitrate':'22050','channels':1}})
-    for timecode in conv:
-        pass    
-    os.system("mpg123 -w audio.wav audio.mp3")
-    wav2txt("audio.wav")
 
-
+## End wav to text
 def main():
     audFile = input("Enter audio file name: ")
-    clip2wav(audFile)
     
+    wav2txt(audFile)
 
 if __name__ == "__main__":
     main()
