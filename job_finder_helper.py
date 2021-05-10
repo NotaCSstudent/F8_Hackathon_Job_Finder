f9211c117333305638bf1e645e2620b9907b7b83
dev-rb <43100342+dev-rb@users.noreply.github.com>
2021-05-09 21:45:40 -0400

    Sending messages to messenger.

--- job_finder_helper.py
+++ job_finder_helper.py
@@ -5,81 +5,80 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-## Begin Job Finder
+# Begin Job Finder
 
 
-def Get_Url(Job :str):
+def Get_Url(Job: str):
     Template = 'https://www.indeed.com/jobs?q={}&l=New+York%2C+NY'
-    
+
     Url = Template.format(Job)
-    
+
     return Url
 
 
+def Find_Job(Job: str, Number_Of_Jobs: int):
 
+    Job_Url = Get_Url(Job)  # For the Helper Function Above
 
+    Job_Response = requests.get(Job_Url)  # Getting a Response from the URL
 
-def Find_Job(Job : str,Number_Of_Jobs :int):
-    
-    Job_Url = Get_Url(Job) ##For the Helper Function Above
-    
-    Job_Response = requests.get(Job_Url) ## Getting a Response from the URL
-    
-    Job_Soup = BeautifulSoup(Job_Response.text,"html.parser") ##Parses into an HTML Tree Structure
-    
-    Name_Of_Role = Job_Soup.find_all('div','jobsearch-SerpJobCard')##Role name tag
-    
+    # Parses into an HTML Tree Structure
+    Job_Soup = BeautifulSoup(Job_Response.text, "html.parser")
+
+    Name_Of_Role = Job_Soup.find_all(
+        'div', 'jobsearch-SerpJobCard')  # Role name tag
+
     Name_Finder = Name_Of_Role[Number_Of_Jobs]
-    
+
     Name = Name_Finder.h2.a
-    
-    Company_Name = Job_Soup.find_all('span','company') ##Finds company html tag
-    
-    Company = Name_Finder.find('span','company').text.strip() ## Gets the company name and strips out anything thats not needed
-    
-    Company_Location = Name_Of_Role[Number_Of_Jobs] ##Starting at index 
 
-    Location = Company_Location.find('div','recJobLoc').get('data-rc-loc')##gets the role
+    Company_Name = Job_Soup.find_all(
+        'span', 'company')  # Finds company html tag
 
-    Details = Name['title'] + "\n" + Company + "\n" + Location
+    # Gets the company name and strips out anything thats not needed
+    Company = Name_Finder.find('span', 'company').text.strip()
 
+    Company_Location = Name_Of_Role[Number_Of_Jobs]  # Starting at index
+
+    Location = Company_Location.find('div', 'recJobLoc').get(
+        'data-rc-loc')  # gets the role
+
+    # Details = Name['title'] + "\n" + Company + "\n" + Location
+    Details = {"name": Name['title'], "company": Company, "location": Location}
     return (Details)
 
 
 #print(len(Find_Job("Software Engineer")))
-##First_Job = Find_Job("Software Engineer",1) 
-#print(len(First_Job))
-##print(First_Job[0])
-#print(First_Job[0].h2)
-#print(First_Job[0].h2.a)
+##First_Job = Find_Job("Software Engineer",1)
+# print(len(First_Job))
+# print(First_Job[0])
+# print(First_Job[0].h2)
+# print(First_Job[0].h2.a)
 #Tag = First_Job[0].h2.a
-#print(Tag['title'])
+# print(Tag['title'])
 
 
-
-
-def Find_My_Job(Insert_Job : str):
+def Find_My_Job(Insert_Job: str):
     Job_List = []
-    for i in range(1,7):
+    for i in range(1, 15):
         try:
-            Job_List.append(Find_Job(Insert_Job,i));
-            print(Find_Job(Insert_Job, i));
+            Job_List.append(Find_Job(Insert_Job, i))
+            # print(Find_Job(Insert_Job, i))
         except(IndexError):
             pass
-        ##Use this to skip any Index Errors
+    return Job_List
+
+    # Use this to skip any Index Errors
 ##Find_My_Job("Software Engineer")
 ##Find_My_Job("Data Analyst")
-##Find_My_Job("Janitor")
+# Find_My_Job("Janitor")
 Find_My_Job("Tutor")
 
 
-## End Job Finder
+# End Job Finder
 
 
-
-
-
-## Begin wav to text
+# Begin wav to text
 
 
 def wav2txt(audioFileName):
@@ -91,26 +90,28 @@ def wav2txt(audioFileName):
     with givenAudioFile as srcFile:
         givenAudioFile = reco.record(srcFile)
 
-    userCommand = (reco.recognize_wit(givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
+    userCommand = (reco.recognize_wit(
+        givenAudioFile, "O7NTCSR3OEK6VZOGW4I65N6P5OTKSI3C"))
     if userCommand.find(' in') > -1:
-        userCommand = userCommand[ (userCommand.find('looking for ')+ 12) : (userCommand.find(' in'))]
+        userCommand = userCommand[(userCommand.find(
+            'looking for ') + 12): (userCommand.find(' in'))]
     else:
-        userCommand = userCommand[ (userCommand.find('looking for ')+ 12) :]
+        userCommand = userCommand[(userCommand.find('looking for ') + 12):]
     if userCommand.find('recent ') > -1:
-        userCommand = userCommand[ (userCommand.find('recent ')+ 7) :]
-    
-    
+        userCommand = userCommand[(userCommand.find('recent ') + 7):]
+
     txtFile = open("audioAsText.txt", "w+")
     txtFile.write(userCommand)
     txtFile.close()
     return userCommand
 
 
-## End wav to text
+# End wav to text
 def main():
     audFile = input("Enter audio file name: ")
-    
+
     wav2txt(audFile)
+
 
 if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import os
+import requests
 import speech_recognition as sr
 from pydub import AudioSegment
 from flask import Flask, request, abort
@@ -10,6 +11,47 @@ import job_finder_helper as jfh
 app = Flask(__name__)
 
 # Install ffmpeg on host/server
+
+# Send a response back to the user
+PAGE_ACCESS_TOKEN = 'EAACMn1ZClEwMBAMP3Mwd434nGLaoyzKFaAcZA1zcJQtTGPjdHLuR36S37TEm9ecl9hIThn1Rbm2eqR3RXE2k7YrpUZBXfjuGtrb6V8zptVo2pZBbvZBTwWquuk6idxR187PhcZC3PnK6ZAOZB8kZAW8YUGhVTW1c3868mVc2KHrC08dxXjy4ZC4uYZC0mD49z5BOgURMg1WLMZBhVNVyA9D04ZAWG'
+
+
+def send_message(recipient_id, text):
+
+    elementsList = []
+
+    for i in range(0, 7):
+        newElement = {"title": text[i]["company"],
+                      "subtitle": text[i]["name"] + " - " + text[i]["location"]}
+        elementsList.append(newElement)
+
+    payload = {
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'attachment': {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": elementsList
+                }
+            }
+        },
+        'notification_type': 'regular'
+    }
+
+    auth = {
+        'access_token': PAGE_ACCESS_TOKEN
+    }
+
+    response = requests.post(
+        "https://graph.facebook.com/v10.0/me/messages?access_token=" + PAGE_ACCESS_TOKEN,
+        params=auth,
+        json=payload
+    )
+
+    return response.json()
 
 
 def messengerClipToWavMac(audioFileName):
@@ -42,14 +84,14 @@ def handleMessage():
     if request.method == 'POST':
         # print(request.json)
         attachmentType = request.json["entry"][0]["messaging"][0]["message"]["attachments"][0]["type"]
-
+        senderId = request.json["entry"][0]["messaging"][0]["sender"]["id"]
         if attachmentType == "audio":
             attachmentUrl = request.json["entry"][0]["messaging"][0]["message"]["attachments"][0]["payload"]["url"]
             urllib.request.urlretrieve(attachmentUrl, "input.mp4")
             messengerClipToWav("input.mp4")
-            job = jfh.wav2text("output.wav")
-            jfh.Find_My_Job(job,0)            
-            
+            job = jfh.wav2txt("output.wav")
+            listOfJobs = jfh.Find_My_Job(job)
+            print(send_message(senderId, listOfJobs))
 
         return 'success', 200
     else:
